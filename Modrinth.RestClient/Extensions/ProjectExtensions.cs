3b87447a24bc31ef1b1f7338a6e34b7240a1f3d8
Zechiax <106590288+Zechiax@users.noreply.github.com>
2022-09-27 01:03:07 +0200

    add project extensions

---
+++ Modrinth.RestClient/Extensions/ProjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Modrinth.RestClient.Models;
+using Version = Modrinth.RestClient.Models.Version;
+
+namespace Modrinth.RestClient.Extensions;
+
+public static class ProjectExtensions
+{
+    public static string GetVersionUrl(this Project project, Version version)
+    {
+        return project.GetVersionUrl(version.Id);
+    }
+    
+    public static string GetVersionUrl(this Project project, string versionId)
+    {
+        return $"{project.Url}/version/{versionId}";
+    }
+}
