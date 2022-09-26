2564621d71a67e8278d7ad25efd1074baf68c9a3
Zechiax <106590288+Zechiax@users.noreply.github.com>
2022-09-27 01:12:36 +0200

    add docs

--- Modrinth.RestClient/Extensions/ProjectExtensions.cs
+++ Modrinth.RestClient/Extensions/ProjectExtensions.cs
@@ -5,11 +5,23 @@ namespace Modrinth.RestClient.Extensions;
 
 public static class ProjectExtensions
 {
+    /// <summary>
+    /// Return URL to version details of specific version
+    /// </summary>
+    /// <param name="project"></param>
+    /// <param name="version">Version for which the URL should be returned</param>
+    /// <returns></returns>
     public static string GetVersionUrl(this Project project, Version version)
     {
         return project.GetVersionUrl(version.Id);
     }
     
+    /// <summary>
+    /// Return URL to version details of specific version
+    /// </summary>
+    /// <param name="project"></param>
+    /// <param name="versionId">ID of the version</param>
+    /// <returns></returns>
     public static string GetVersionUrl(this Project project, string versionId)
     {
         return $"{project.Url}/version/{versionId}";
--- Modrinth.RestClient/Extensions/VersionExtensions.cs
+++ Modrinth.RestClient/Extensions/VersionExtensions.cs
@@ -5,6 +5,12 @@ namespace Modrinth.RestClient.Extensions;
 
 public static class VersionExtensions
 {
+    /// <summary>
+    /// Returns the URL to this version details
+    /// </summary>
+    /// <param name="version"></param>
+    /// <param name="project">The project to which this version belongs</param>
+    /// <returns></returns>
     public static string GetUrl(this Version version, Project project)
     {
         return project.GetVersionUrl(version);
