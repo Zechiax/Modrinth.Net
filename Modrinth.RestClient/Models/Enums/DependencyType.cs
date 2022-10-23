caab84362565a5c0247575d22057b66dffeb5544
Zechiax <106590288+Zechiax@users.noreply.github.com>
2022-10-23 12:10:58 +0200

    add more docs

--- Modrinth.RestClient/Models/Enums/DependencyType.cs
+++ Modrinth.RestClient/Models/Enums/DependencyType.cs
@@ -2,8 +2,20 @@
 
 public enum DependencyType
 {
+    /// <summary>
+    /// Dependency is required
+    /// </summary>
     Required,
+    /// <summary>
+    /// Dependency is optional
+    /// </summary>
     Optional,
+    /// <summary>
+    /// Dependency is incompatible
+    /// </summary>
     Incompatible,
+    /// <summary>
+    /// Dependency is embedded
+    /// </summary>
     Embedded
 }
