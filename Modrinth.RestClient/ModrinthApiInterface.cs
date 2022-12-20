1343863a982fc8b808e6a2e009a1362d23246e59
Zechiax <106590288+Zechiax@users.noreply.github.com>
2022-12-20 19:48:24 +0100

    Change default index value

--- Modrinth.RestClient/ModrinthApiInterface.cs
+++ Modrinth.RestClient/ModrinthApiInterface.cs
@@ -69,7 +69,7 @@ public interface IModrinthApi
     [Get("search")]
     Task<SearchResponse> SearchProjectsAsync(
         [Query("query")] string query,
-        [Query("index")] Index index = Index.Downloads,
+        [Query("index")] Index index = Index.Relevance,
         [Query("offset")] ulong offset = 0,
         [Query("limit")] ulong limit = 10);
     
