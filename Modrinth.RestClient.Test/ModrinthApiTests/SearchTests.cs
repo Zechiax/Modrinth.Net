8c98cba155022e912dd41f586d8fe2afa790e470
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-01-23 22:32:32 +0100

    add tests for various search parameters

--- Modrinth.RestClient.Test/ModrinthApiTests/SearchTests.cs
+++ Modrinth.RestClient.Test/ModrinthApiTests/SearchTests.cs
@@ -1,5 +1,8 @@
-﻿namespace Modrinth.RestClient.Test.ModrinthApiTests;
+﻿using Index = Modrinth.RestClient.Models.Enums.Index;
 
+namespace Modrinth.RestClient.Test.ModrinthApiTests;
+
+[TestFixture]
 public class SearchTests : EndpointTests
 {
     [Test]
@@ -41,9 +44,85 @@ public class SearchTests : EndpointTests
         Assert.Multiple(() =>
         {
             Assert.That(search.TotalHits, Is.GreaterThan(0));
-            Assert.That(search.Hits, Is.Not.Null);
-            Assert.That(search.Hits, Is.Not.Empty);
             Assert.That(search.Hits.Count, Is.EqualTo(limit));
+            Assert.That(search.Hits, Is.Not.Null);
+            if (limit == 0)
+            {
+                Assert.That(search.Hits, Is.Empty);
+                return;
+            }
+            Assert.That(search.Hits, Is.Not.Empty);
+            
         });
+    }
+    
+    // Test different offset values
+    [Test]
+    [TestCase((ulong) 0)]
+    [TestCase((ulong) 5)]
+    [TestCase((ulong) 10)]
+    [TestCase((ulong) 15)]
+    [TestCase((ulong) 20)]
+    public async Task Search_WithOffset_ShouldReturnOffsetList(ulong offset)
+    {
+        var search = await _client.Project.SearchAsync("", limit: offset + 5);
+        var searchWithOffset = await _client.Project.SearchAsync("", offset: offset, limit: offset + 5);
+        
+        // Check that the offset list is not the same as the original list
+        Assert.That(searchWithOffset.Hits, Is.Not.EqualTo(search.Hits));
+        
+        // Check that the offset list is the same as the original list with the first 5 elements removed
+        // Check the ids of the first 5 elements
+        Assert.That(
+            searchWithOffset.Hits.Select(p => p.ProjectId).Take(5), 
+            Is.EqualTo(search.Hits.Select(p => p.ProjectId).Skip((int)offset).Take(5)));
+    }
+    
+    // Test download sorting
+    [Test]
+    public async Task Search_WithDownloadsSort_ShouldReturnSortedByDownloadsList()
+    {
+        var search = await _client.Project.SearchAsync("", index: Index.Downloads);
+
+        // Check that the list is sorted by downloads
+        Assert.That(
+            search.Hits.Select(p => p.Downloads), 
+            Is.EqualTo(search.Hits.Select(p => p.Downloads).OrderByDescending(d => d)));
+    }
+    
+    // Test followers sorting
+    [Test]
+    public async Task Search_WithFollowersSort_ShouldReturnSortedByFollowersList()
+    {
+        var search = await _client.Project.SearchAsync("", index: Index.Follows);
+
+        // Check that the list is sorted by followers
+        Assert.That(
+            search.Hits.Select(p => p.Followers), 
+            Is.EqualTo(search.Hits.Select(p => p.Followers).OrderByDescending(d => d)));
+    }
+    
+    // Test newest sorting
+    [Test]
+    public async Task Search_WithNewestSort_ShouldReturnSortedByNewestList()
+    {
+        var search = await _client.Project.SearchAsync("", index: Index.Newest);
+
+        // Check that the list is sorted by newest
+        Assert.That(
+            search.Hits.Select(p => p.DateCreated), 
+            Is.EqualTo(search.Hits.Select(p => p.DateCreated).OrderByDescending(d => d)));
+    }
+    
+    // Test updated sorting
+    [Test]
+    public async Task Search_WithUpdatedSort_ShouldReturnSortedByUpdatedList()
+    {
+        var search = await _client.Project.SearchAsync("", index: Index.Updated);
+
+        // Check that the list is sorted by updated
+        Assert.That(
+            search.Hits.Select(p => p.DateModified), 
+            Is.EqualTo(search.Hits.Select(p => p.DateModified).OrderByDescending(d => d)));
     }
 }
