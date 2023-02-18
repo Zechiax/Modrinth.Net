a8f8561913dd27dd9a121a72aeccb5134d6ea3d2
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-02-18 15:51:28 +0100

    add test for stats

--- Modrinth.Net/Endpoints/Miscellaneous/ModrinthStatistics.cs
+++ Modrinth.Net/Endpoints/Miscellaneous/ModrinthStatistics.cs
@@ -2,5 +2,20 @@
 
 public class ModrinthStatistics
 {
-    
+    /// <summary>
+    /// Number of projects on Modrinth
+    /// </summary>
+    public int Projects { get; set; }
+    /// <summary>
+    ///   Number of versions on Modrinth
+    /// </summary>
+    public int Versions { get; set; }
+    /// <summary>
+    /// Number of version files on Modrinth
+    /// </summary>
+    public int Files { get; set; }
+    /// <summary>
+    /// Number of authors (users with projects) on Modrinth
+    /// </summary>
+    public int Authors { get; set; }
 }
