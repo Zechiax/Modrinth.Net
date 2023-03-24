45dd49ad1a477693a12219bdbccf05de0609823f
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-03-24 15:44:57 +0100

    tests of client configuration

---
+++ Modrinth.Net.Test/ModrinthClientConfigTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Modrinth.Net.Test;
+
+[TestFixture]
+public class ModrinthClientConfigTests
+{
+    [Test]
+    public void DefaultUserAgentIsPresent()
+    {
+        var config = new ModrinthClientConfig();
+
+        Assert.That(config.UserAgent, Does.StartWith("Modrinth.Net"));
+    }
+
+    [Test]
+    public void UserAgentCanBeSet()
+    {
+        var config = new ModrinthClientConfig
+        {
+            UserAgent = "Test"
+        };
+
+        Assert.That(config.UserAgent, Is.EqualTo("Test"));
+    }
+
+    [Test]
+    public void ConfigCorrectlySetsUserAgent()
+    {
+        using var httpClient = new HttpClient();
+
+        var config = new ModrinthClientConfig
+        {
+            UserAgent = "Test"
+        };
+
+        using var client = new ModrinthClient(config, httpClient);
+
+        Assert.That(httpClient.DefaultRequestHeaders.UserAgent.ToString(), Is.EqualTo("Test"));
+    }
+}
