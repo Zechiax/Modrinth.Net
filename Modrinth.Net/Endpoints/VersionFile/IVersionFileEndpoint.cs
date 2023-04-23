e509440d7342c6dbad2782e723ac1b1de2ae4dd7
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-04-23 20:39:23 +0200

    better docs for the version file endpoints

--- Modrinth.Net/Endpoints/VersionFile/IVersionFileEndpoint.cs
+++ Modrinth.Net/Endpoints/VersionFile/IVersionFileEndpoint.cs
@@ -9,12 +9,18 @@ public interface IVersionFileEndpoint
     ///     Get specific version by file hash
     /// </summary>
     /// <param name="hash">The hash of the file, considering its byte content, and encoded in hexadecimal</param>
-    /// <param name="hashAlgorithm"></param>
+    /// <param name="hashAlgorithm"> The hash algorithm used to generate the hash </param>
     /// <returns></returns>
     /// <exception cref="ModrinthApiException"> Thrown when the API returns an error or the request fails </exception>
     Task<System.Version> GetVersionByHashAsync(string hash,
         HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1);
 
+    /// <summary>
+    ///    Deletes a version by its file hash
+    /// </summary>
+    /// <param name="hash"> The hash of the file, considering its byte content, and encoded in hexadecimal </param>
+    /// <param name="hashAlgorithm"> The hash algorithm used to generate the hash </param>
+    /// <returns></returns>
     Task DeleteVersionByHashAsync(string hash,
         HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1);
 }
