dbac605721b8e565161f9b944ea5253582040694
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-05-13 23:13:38 +0200

    implementation of getting multiple latest versions by hash

--- Modrinth.Net/Endpoints/VersionFile/IVersionFileEndpoint.cs
+++ Modrinth.Net/Endpoints/VersionFile/IVersionFileEndpoint.cs
@@ -46,4 +46,15 @@ public interface IVersionFileEndpoint
     /// <returns> The latest version of a project, that matches the filters </returns>
     Task<Models.Version> GetLatestVersionByHashAsync(string hash, HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1, 
         string[]? loaders = null, string[]? gameVersions = null);
+    
+    /// <summary>
+    ///  Gets the latest version of multiple projects by a file hash
+    /// </summary>
+    /// <param name="hashes"> The hashes of the files, considering their byte content, and encoded in hexadecimal </param>
+    /// <param name="hashAlgorithm"> The hash algorithm used to generate the hash </param>
+    /// <param name="loaders"> The loaders to filter by </param>
+    /// <param name="gameVersions"> The game versions to filter by </param>
+    /// <returns> A dictionary of hashes and their respective latest versions that match the filters </returns>
+    Task<IDictionary<string, Models.Version>> GetMultipleLatestVersionsByHashAsync(string[] hashes, HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1, 
+        string[]? loaders = null, string[]? gameVersions = null);
 }
--- Modrinth.Net/Endpoints/VersionFile/VersionFileEndpoint.cs
+++ Modrinth.Net/Endpoints/VersionFile/VersionFileEndpoint.cs
@@ -99,4 +99,29 @@ public class VersionFileEndpoint : Endpoint, IVersionFileEndpoint
         
         return await Requester.GetJsonAsync<Models.Version>(reqMsg).ConfigureAwait(false);
     }
+
+    /// <inheritdoc />
+    public async Task<IDictionary<string, Models.Version>> GetMultipleLatestVersionsByHashAsync(string[] hashes, HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1,
+        string[]? loaders = null, string[]? gameVersions = null)
+    {
+        loaders = loaders ?? Array.Empty<string>();
+        gameVersions = gameVersions ?? Array.Empty<string>();
+        
+        var reqMsg = new HttpRequestMessage();
+        reqMsg.Method = HttpMethod.Post;
+        reqMsg.RequestUri = new Uri("version_files/update", UriKind.Relative);
+        
+        // Info in request body application/json
+        var requestBody = new
+        {
+            algorithm = hashAlgorithm.ToString().ToLower(),
+            hashes,
+            loaders,
+            game_versions = gameVersions,
+        };
+        
+        reqMsg.Content = new StringContent(JsonSerializer.Serialize(requestBody), Encoding.UTF8, "application/json");
+        
+        return await Requester.GetJsonAsync<IDictionary<string, Models.Version>>(reqMsg).ConfigureAwait(false);
+    }
 }
