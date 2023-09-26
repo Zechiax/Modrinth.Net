16aeeacfcfc2512c3b546ba1043b515b2c5669e2
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-09-26 20:27:57 +0200

    cancellation token support for version file endpoint

--- src/Modrinth.Net/Endpoints/VersionFile/IVersionFileEndpoint.cs
+++ src/Modrinth.Net/Endpoints/VersionFile/IVersionFileEndpoint.cs
@@ -13,28 +13,31 @@ public interface IVersionFileEndpoint
     /// </summary>
     /// <param name="hash">The hash of the file, considering its byte content, and encoded in hexadecimal</param>
     /// <param name="hashAlgorithm"> The hash algorithm used to generate the hash </param>
+    /// <param name="cancellationToken"> The cancellation token to cancel operation </param>
     /// <returns></returns>
     /// <exception cref="ModrinthApiException"> Thrown when the API returns an error or the request fails </exception>
     Task<Models.Version> GetVersionByHashAsync(string hash,
-        HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1);
+        HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1, CancellationToken cancellationToken = default);
 
     /// <summary>
     ///     Deletes a version by its file hash
     /// </summary>
     /// <param name="hash"> The hash of the file, considering its byte content, and encoded in hexadecimal </param>
     /// <param name="hashAlgorithm"> The hash algorithm used to generate the hash </param>
+    /// <param name="cancellationToken"> The cancellation token to cancel operation </param>
     /// <returns></returns>
     Task DeleteVersionByHashAsync(string hash,
-        HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1);
+        HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1, CancellationToken cancellationToken = default);
 
     /// <summary>
     ///     Get multiple versions by file hash
     /// </summary>
     /// <param name="hashes"> The hashes of the files, considering their byte content, and encoded in hexadecimal </param>
     /// <param name="hashAlgorithm"> The hash algorithm used to generate the hash </param>
+    /// <param name="cancellationToken"> The cancellation token to cancel operation </param>
     /// <returns> A dictionary of hashes and their respective versions </returns>
     Task<IDictionary<string, Models.Version>> GetMultipleVersionsByHashAsync(string[] hashes,
-        HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1);
+        HashAlgorithm hashAlgorithm = HashAlgorithm.Sha1, CancellationToken cancellationToken = default);
 
     /// <summary>
     ///     Gets the latest version of a projects by a file hash
@@ -43,9 +46,10 @@ public interface IVersionFileEndpoint
     /// <param name="hashAlgorithm"> The hash algorithm used to generate the hash </param>
     /// <param name="loaders"> The loaders to filter by </param>
     /// <param name="gameVersions"> The game versions to filter by </param>
+    /// <param name="cancellationToken"> The cancellation token to cancel operation </param>
     /// <returns> The latest version of a project, that matches the filters </returns>
     Task<Models.Version> GetLatestVersionByHashAsync(string hash, HashAlgorithm hashAlgorithm,
-        string[] loaders, string[] gameVersions);
+        string[] loaders, string[] gameVersions, CancellationToken cancellationToken = default);
 
     /// <summary>
     ///     Gets the latest version of multiple projects by a file hash
@@ -54,8 +58,9 @@ public interface IVersionFileEndpoint
     /// <param name="hashAlgorithm"> The hash algorithm used to generate the hash </param>
     /// <param name="loaders"> The loaders to filter by </param>
     /// <param name="gameVersions"> The game versions to filter by </param>
+    /// <param name="cancellationToken"> The cancellation token to cancel operation </param>
     /// <returns> A dictionary of hashes and their respective latest versions that match the filters </returns>
     Task<IDictionary<string, Models.Version>> GetMultipleLatestVersionsByHashAsync(string[] hashes,
         HashAlgorithm hashAlgorithm,
-        string[] loaders, string[] gameVersions);
+        string[] loaders, string[] gameVersions, CancellationToken cancellationToken = default);
 }
